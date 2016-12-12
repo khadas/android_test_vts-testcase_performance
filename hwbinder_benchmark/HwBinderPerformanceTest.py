2b6f76a082d46c80f8a318292094d98bef20eb94
Keun Soo Yim <yim@google.com>
2016-12-12 14:44:53 -0800

    stop the framework during a performance test
    Test: make vts BUILD_GOOGLE_VTS=true -j32 && vts-tradefed run commandAndExit vts --skip-all-system-status-check --primary-abi-only --skip-preconditions --module HwBinderPassthroughPerformanceTest -l INFO
    Change-Id: I3fb7db610b7876447b29dc22215c307ea01fdb74
    Fixes: 33749565

--- hwbinder_benchmark/HwBinderPerformanceTest.py
+++ hwbinder_benchmark/HwBinderPerformanceTest.py
@@ -71,10 +71,14 @@ class HwBinderPerformanceTest(base_test_with_webdb.BaseTestWithWebDbClass):
         self.getUserParams(required_params)
         self.dut = self.registerController(android_device)[0]
         self.dut.shell.InvokeTerminal("one")
+        self.dut.shell.one.Execute("stop")
+        self.dut.shell.one.Execute("setprop sys.boot_completed 0")
         self.DisableCpuScaling()
 
     def tearDownClass(self):
         self.EnableCpuScaling()
+        self.dut.shell.one.Execute("start")
+        self.dut.waitForBootCompletion()
 
     def ChangeCpuGoverner(self, mode):
         """Changes the cpu governer mode of all the cpus on the device.
