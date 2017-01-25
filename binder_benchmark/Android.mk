8ee7ad3a95d51aea2368703553bb1722eaa31a00
Yuexi Ma <yuexima@google.com>
2017-01-25 02:28:38 -0800

    Create performance benchmark with systrace test plan
    * split test plans into regular test and test with systrace
    * add missing test-module-names, correct indents
    
    Bug: 34696301
    Test: ENABLE_TREBLE=true make vts BUILD_GOOGLE_VTS=true -j32 && vts-tradefed run commandAndExit vts --skip-all-system-status-check --primary-abi-only --skip-preconditions --module BinderPerformanceTest -l INFO
    Change-Id: I29a46336d555fcdc739637fdf5341e0dc00a4dcc
    (cherry picked from commit cc9fcbb26d03c74d3574ab7c17137fc797e715ae)

--- binder_benchmark/Android.mk
+++ binder_benchmark/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2016 The Android Open Source Project
+# Copyright (C) 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,9 +16,3 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(call all-subdir-makefiles)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := BinderPerformanceTest
-VTS_CONFIG_SRC_DIR := testcases/performance/binder_benchmark
-include test/vts/tools/build/Android.host_config.mk
